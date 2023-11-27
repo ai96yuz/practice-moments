6c1e5ed5eb7cd582893392f2486231ef221548f6
ai96yuz <ai96yuz@gmail.com>
2023-11-27 17:57:32 +0100

    add selenium

---
+++ test.py
@@ -0,0 +1,36 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from constants import *
+
+
+# driver = webdriver.Firefox()
+
+driver = webdriver.Chrome()
+driver.get("https://visa.vfsglobal.com/blr/ru/pol/login")
+time.sleep(10)
+elem = driver.find_element(By.XPATH, '//*[@id="mat-input-0"]')
+elem.send_keys(username)
+time.sleep(5)
+elem = driver.find_element(By.XPATH, '//*[@id="mat-input-1"]')
+elem.send_keys(password)
+time.sleep(4)
+
+checkbox = driver.find_element(By.XPATH, '//*[@id="challenge-stage"]')
+
+if not checkbox.is_selected():
+    checkbox.click()
+
+time.sleep(15)
+checkbox.click()
+
+time.sleep(15)
+checkbox.click()
+# submit_button = driver.find_element(By.ID, "submit")
+# submit_button.click()
+
+time.sleep(60)
+
+driver.close()
