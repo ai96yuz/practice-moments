968eed56621465d607eae0dba759a3cb3008fe2d
ai96yuz <ai96yuz@gmail.com>
2023-11-27 18:43:40 +0100

    added simple password generator

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 
 from constants import *
-
+from solver import *
 
 # driver = webdriver.Firefox()
 
